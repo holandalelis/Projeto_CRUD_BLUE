41ed36ce105518e474b8d82472d9a31c234ed678
pedro <pedrolelis@hotmail.com>
2022-02-23 16:45:04 -0300

    altera metodo excluir no homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -49,7 +49,7 @@ namespace UsandoViews.Controllers
         [HttpPost]
         public IActionResult Excluir(Usuario usuario)
         {
-            Usuario.Excluir(usuario.IdUsuario);
+            ViewBag.Excluiu = Usuario.Excluir(usuario.IdUsuario);
             return RedirectToAction("Usuarios");
         }
     }
